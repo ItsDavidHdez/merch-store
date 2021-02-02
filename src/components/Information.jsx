1db0ad4f418a4993faa2dc96d93ece07e5a97a5a
David Vargas Hernandez <david-0610@live.com.mx>
2021-02-02 02:46:12 -0600

    Information component created

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/components/Information.css';
 
 const Information = () => {
   return (
-    <>
-      <h1>Information</h1>
-    </>
+    <div className="Information">
+      <div className="Information-content">
+        <div className="Information-head">
+          <h2>Información de contacto:</h2>
+        </div>
+        <div className="Information-form">
+          <form action="">
+            <input type="text" placeholder="Nombre completo" name="name" />
+            <input type="text" placeholder="Correo Electronico" name="email" />
+            <input type="text" placeholder="Dirección" name="address" />
+            <input type="text" placeholder="Apto" name="apto" />
+            <input type="text" placeholder="Ciudad" name="city" />
+            <input type="text" placeholder="Pais" name="country" />
+            <input type="text" placeholder="Estado" name="state" />
+            <input type="text" placeholder="Código Postal" name="cp" />
+            <input type="text" placeholder="Télefono" name="phone" />
+          </form>
+        </div>
+        <div className="Information-buttons">
+          <Link to="/checkout">
+            <div className="Information-back">Regresar</div>
+          </Link>
+          <Link to="/checkout/payment">
+            <div className="Information-next">Pagar</div>
+          </Link>
+        </div>
+      </div>
+      <div className="Information-sidebar">
+        <h3>Pedido:</h3>
+        <div className="Information-item">
+          <div className="Information-element">
+            <h4>ITEM NAME</h4>
+            <span>$2000000000</span>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
