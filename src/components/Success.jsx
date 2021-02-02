4585f2b38f96d9a85ec0087a0530e144f08539b3
David Vargas Hernandez <david-0610@live.com.mx>
2021-02-02 02:44:59 -0600

    Success component created

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import '../styles/components/Success.css';
 
 const Success = () => {
   return (
-    <>
-      <h1>Success</h1>
-    </>
+    <div className="Success">
+      <div className="Success-content">
+        <h2>*David*, Gracias por tu compra</h2>
+        <span>Tu pedido llegará en N días a tu dirección</span>
+        <div className="Success-map">***Google MAPS***</div>
+      </div>
+    </div>
   );
 };
 
