8e15ad65e10253be1c0790361fce30d455cb1939
David Vargas Hernandez <david-0610@live.com.mx>
2021-02-03 14:33:25 -0600

    useContext custom

---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import initialState from '../initialState';
+
+const useInitialState = () => {
+  const [state, setState] = useState(initialState);
+
+  const addToCart = (payload) => {
+    setState({
+      ...state,
+      cart: [...state.cart, payload],
+    });
+  };
+
+  const removeFromCart = (payload, index) => {
+    setState({
+      ...state,
+      cart: state.cart.filter((items, key) => key !== index),
+    });
+  };
+
+  const addToBuyer = (payload) => {
+    setState({
+      ...state,
+      buyer: [...state.buyer, payload],
+    });
+  };
+
+  return {
+    addToCart,
+    removeFromCart,
+    state,
+    addToBuyer,
+  };
+};
+
+export default useInitialState;
